f427b63a3521c504d2e501d677931d7c2bc84e79
williamberg mariano <123568971+williambergnx@users.noreply.github.com>
2023-03-31 21:35:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+    <center>
+        <form id="login"  action="aulafor.php">
+            <input type="submit" name="entrar" value="entrar">
+</form>
+    </center>
+</body>
+</html>
