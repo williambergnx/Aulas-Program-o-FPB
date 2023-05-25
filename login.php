c4886c4dc57445ae80f7d3862f2d03519a35c932
williamberg mariano <123568971+williambergnx@users.noreply.github.com>
2023-05-25 00:24:46 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+    <meta chaset="utf-8"/>
+    <title>Tela de Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div id="corpo-form">    
+    <h1>Entrar</h1>
+    <form method="POST" action="processa.php">
+        <input type="email" placeholder="Usuário">
+        <input type="password"placeholder="Senha">
+        <input type="submit" value="ACESSAR">
+        <a href="">Ainda não está cadastrado?<strong> Cadastre-se aqui</a>
+    </form>
+</div>
+</body
+</html>
